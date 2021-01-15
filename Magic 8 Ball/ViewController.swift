9f66e00759c76dbf1d0ab1f106b2c44f15ccaa86
Thin Myat Noe <thinmyatnoe@Thins-MacBook-Pro.local>
2021-01-15 16:59:14 +1100

    finished project

---
+++ Magic 8 Ball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Angela Yu on 14/06/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+
+class ViewController: UIViewController {
+    @IBOutlet weak var imgView1: UIImageView!
+    @IBAction func askBtnPressed(_ sender: Any) {
+        imgView1.image = ballArray[Int.random(in: 0...4)]
+    }
+}
+
